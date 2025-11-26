827473458284959ac033cd4c6ac1e34794f91a3b
4justo <symononoka78@gmail.com>
2025-11-26 17:10:06 +0300

    Add files via upload

---
+++ week2variables/src/Main.java
@@ -0,0 +1,159 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        final int num_units =  7; //fixed number of units
+
+        //Variable declaration
+        //**************Student 1********************
+        String regno1, studentname1;
+        System.out.print("Enter  First Student Registration No : ");
+        regno1 = input.nextLine();
+        System.out.print("Enter  First Student name : ");
+        studentname1 = input.nextLine();
+        int CCS2211S1, CCS2212S1, CCS2213S1, CCS2214S1, CCS2215S1, CCS2216S1, CCS2217S1, Totals1;
+        System. out.print("Enter marks for CCS2211: ");
+        CCS2211S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2212: ");
+        CCS2212S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2213: ");
+        CCS2213S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2214: ");
+        CCS2214S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2215: ");
+        CCS2215S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2216: ");
+        CCS2216S1 = input.nextInt();
+        System. out.print("Enter marks for CCS2217: ");
+        CCS2217S1 = input.nextInt();
+        Totals1 = CCS2211S1+CCS2212S1+CCS2213S1+CCS2214S1+CCS2215S1+CCS2216S1+CCS2217S1;
+        float avarage1;
+        avarage1 = (float) Totals1 / num_units;
+        input.nextLine();
+
+        //***************Student2**********************
+
+        String regno2, studentname2;
+        System.out.print("Enter  Second Student Registration No : ");
+        regno2 = input.nextLine();
+        System.out.print("Enter  Second Student name : ");
+        studentname2 = input.nextLine();
+        int CCS2211S2, CCS2212S2, CCS2213S2, CCS2214S2,CCS2215S2, CCS2216S2, CCS2217S2, Totals2;
+        System. out.print("Enter marks for CCS2211: ");
+        CCS2211S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2212: ");
+        CCS2212S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2213: ");
+        CCS2213S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2214: ");
+        CCS2214S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2215: ");
+        CCS2215S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2216: ");
+        CCS2216S2 = input.nextInt();
+        System. out.print("Enter marks for CCS2217: ");
+        CCS2217S2 = input.nextInt();
+        Totals2 = CCS2211S2+CCS2212S2+CCS2213S2+CCS2214S2+CCS2215S2+CCS2216S2+CCS2217S2;
+        float avarage2 = (float) Totals2 / num_units;
+        input.nextLine();
+
+        //**************Student 3***********************
+
+        String regno3, studentname3;
+        System.out.print("Enter  Third Student Registration No : ");
+        regno3 = input.nextLine();
+        System.out.print("Enter  Third Student name : ");
+        studentname3 = input.nextLine();
+        int CCS2211S3, CCS2212S3, CCS2213S3, CCS2214S3,CCS2215S3, CCS2216S3, CCS2217S3, Totals3;
+        System. out.print("Enter marks for CCS2211: ");
+        CCS2211S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2212: ");
+        CCS2212S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2213: ");
+        CCS2213S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2214: ");
+        CCS2214S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2215: ");
+        CCS2215S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2216: ");
+        CCS2216S3 = input.nextInt();
+        System. out.print("Enter marks for CCS2217: ");
+        CCS2217S3 = input.nextInt();
+        Totals3 = CCS2211S3+CCS2212S3+CCS2213S3+CCS2214S3+CCS2215S3+CCS2216S3+CCS2217S3;
+        float avarage3;
+        avarage3 = Totals3 / num_units;
+        input.nextLine();
+
+        //*****************Student 4**********************
+        String regno4, studentname4;
+        System.out.print("Enter  Fourth Student Registration No : ");
+        regno4 = input.nextLine();
+        System.out.print("Enter  Forth Student name : ");
+        studentname4 = input.nextLine();
+        int CCS2211S4, CCS2212S4, CCS2213S4, CCS2214S4, CCS2216S4,CCS2215S4, CCS2217S4, Totals4;
+        System. out.print("Enter marks for CCS2211: ");
+        CCS2211S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2212: ");
+        CCS2212S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2213: ");
+        CCS2213S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2214: ");
+        CCS2214S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2215: ");
+        CCS2215S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2216: ");
+        CCS2216S4 = input.nextInt();
+        System. out.print("Enter marks for CCS2217: ");
+        CCS2217S4 = input.nextInt();
+        Totals4 = CCS2211S4+CCS2212S4+CCS2213S4+CCS2214S4+CCS2215S4+CCS2216S4+CCS2217S4;
+        float avarage4;
+        avarage4 = Totals4 / num_units;
+        input.nextLine();
+
+        //********************Student5************************
+        String regno5, studentname5;
+        System.out.print("Enter  Fifth Student Registration No : ");
+        regno5 = input.nextLine();
+        System.out.print("Enter  Fifth Student name : ");
+        studentname5 = input.nextLine();
+        int CCS2211S5, CCS2212S5, CCS2213S5, CCS2214S5,CCS2215S5, CCS2216S5, CCS2217S5, Totals5;
+        System. out.print("Enter marks for CCS2211: ");
+        CCS2211S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2212: ");
+        CCS2212S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2213: ");
+        CCS2213S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2214: ");
+        CCS2214S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2215: ");
+        CCS2215S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2216: ");
+        CCS2216S5 = input.nextInt();
+        System. out.print("Enter marks for CCS2217: ");
+        CCS2217S5 = input.nextInt();
+        Totals5 = CCS2211S5+CCS2212S5+CCS2213S5+CCS2214S5+CCS2215S5+CCS2216S5+CCS2217S5;
+        float avarage5;
+        avarage5 = (float) Totals5 / num_units;
+        input.nextLine();
+
+        // The grid to display the student  marksheet
+        System. out.println("\n\n****************************************************************STUDENT MARKSHEET*********************************************************************************");
+        System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t\tDedan Kimathi University of Technology");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  School of Computer Science and IT");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\tDepartment of Computer Science");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t  Academic year: 2024/2025");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\tSTUDENT MARKSHEET(SECOND YEAR RESULTS)");
+        System.out.println("\n**********************************************************************************************************************************************************************************");
+        System.out.println("\n Student score sheet");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Reg No.\t\tFull Name \t\t\t\t CCS2211 \t CCS2212 \t CCS2213 \t CCS2214 \t CCS2215 \t CCS2216 \t CCS2217 \t TOTALS \t AVARAGE \t\t\t STATUS \t GRADE");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(regno1+ " \t\t"+studentname1 + "\t\t\t\t\t"+ CCS2211S1 + "\t\t\t" + CCS2212S1 + "\t\t\t" + CCS2213S1 + "\t\t\t" + CCS2214S1 +"\t\t\t" +CCS2215S1 + "\t\t\t" + CCS2216S1 + "\t\t\t" + CCS2217S1 + "\t\t\t"+ Totals1 + "\t\t\t"+ avarage1);
+        System.out.println(regno2+ " \t\t"+studentname2 + "\t\t\t\t\t"+ CCS2211S2 + "\t\t\t" + CCS2212S2 + "\t\t\t" + CCS2213S2 + "\t\t\t" + CCS2214S2 +"\t\t\t" +CCS2215S2 + "\t\t\t" + CCS2216S2 + "\t\t\t" + CCS2217S2 + "\t\t\t"+ Totals2 + "\t\t\t"+ avarage2);
+        System.out.println(regno3+ " \t\t"+studentname3 + "\t\t\t\t\t"+ CCS2211S3 + "\t\t\t" + CCS2212S3 + "\t\t\t" + CCS2213S3 + "\t\t\t" + CCS2214S3 +"\t\t\t" +CCS2215S3 + "\t\t\t" + CCS2216S3 + "\t\t\t" + CCS2217S3 + "\t\t\t"+ Totals3 + "\t\t\t"+ avarage3);
+        System.out.println(regno4+ " \t\t"+studentname4 + "\t\t\t\t\t"+ CCS2211S4 + "\t\t\t" + CCS2212S4 + "\t\t\t" + CCS2213S4 + "\t\t\t" + CCS2214S4 +"\t\t\t" +CCS2215S4 + "\t\t\t" + CCS2216S4 + "\t\t\t" + CCS2217S4 + "\t\t\t"+ Totals4 + "\t\t\t"+ avarage4);
+        System.out.println(regno5+ " \t\t"+studentname5 + "\t\t\t\t\t"+ CCS2211S5 + "\t\t\t" + CCS2212S5 + "\t\t\t" + CCS2213S5 + "\t\t\t" + CCS2214S5 +"\t\t\t" +CCS2215S5 + "\t\t\t" + CCS2216S5 + "\t\t\t" + CCS2217S5 + "\t\t\t"+ Totals5 + "\t\t\t"+ avarage5);
+
+    }
+}
